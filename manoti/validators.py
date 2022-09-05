b3a4f309b3da4e527fa83d7c01438b93fce7ebf8
franck kavākuré <cynaptix@gmail.com>
2022-09-05 17:31:42 +0200

    Update validators.py
    cleaning u[ the code

--- manoti/validators.py
+++ manoti/validators.py
@@ -1,26 +1,25 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import get_language, ugettext, ugettext_lazy as _
 
+import os
+from django.core.exceptions import ValidationError
+	
 def validate_file_size(value):
-    filesize = value.size
-    
-    if filesize > 2097152:
-        raise ValidationError(_('The maximum file size that can be uploaded is 2MB'))
-    else:
-        return value
+	filesize = value.size
+	
+	if filesize > 2097152:
+		raise ValidationError(_('The maximum file size that can be uploaded is 2MB'))
+	else:
+		return value
 
 def validate_image_file_extension(value):
-    import os
-    from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.jpg', '.png']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(_('Sorry! Only .jpg or .png images are accepted'))
+	ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+	valid_extensions = ['.jpg', '.png']
+	if not ext.lower() in valid_extensions:
+		raise ValidationError(_('Sorry! Only .jpg or .png images are accepted'))
 
 def validate_document_file_extension(value):
-    import os
-    from django.core.exceptions import ValidationError
-    ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
-    valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.xlsx', '.xls']
-    if not ext.lower() in valid_extensions:
-        raise ValidationError(_('Sorry: Unsupported file! Only .pdf, .doc, .docx, .jpg, .png, .xlsx, .xls files are accepted'))
+	ext = os.path.splitext(value.name)[1]  # [0] returns path+filename
+	valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.xlsx', '.xls']
+	if not ext.lower() in valid_extensions:
+		raise ValidationError(_('Sorry: Unsupported file! Only .pdf, .doc, .docx, .jpg, .png, .xlsx, .xls files are accepted'))
