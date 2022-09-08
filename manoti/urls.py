a01960067c7fe4781781442478d7bbd3208e1d29
franck kavakure <cynaptix@gmail.com>
2022-09-08 21:29:00 +0200

    Update urls.py
    fixing URL schemes

--- manoti/urls.py
+++ manoti/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.dahshboard, name='dashboard'),
-    url(r'^commerce/$', views.third_party_homepage, name='third_party_homepage'),
-    url(r'^thirdparties/$', views.list_third_parties, name='list_third_parties'),
+    url(r'^third-parties/$', views.third_party_homepage, name='third_party_homepage'),
+    url(r'^third-party-list/$', views.list_third_parties, name='list_third_parties'),
 ]
