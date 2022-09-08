d0c582e762397eeb60cfbc6a80a3056734441b6c
franck kavākuré <cynaptix@gmail.com>
2022-09-08 17:01:06 +0200

    Update urls.py

--- manoticrm/urls.py
+++ manoticrm/urls.py
@@ -1,23 +1,20 @@
-from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import include, url
+from django.urls import path
+from django.conf.urls.static import static
 from django.contrib import admin
+from hello import views, urls
+from manoti import views, urls
 
 admin.autodiscover()
 
-import hello.views
-import manoti.views
-
-# To add a new path, first import the app:
-# import blog
-#
-# Then add the new path:
-# path('blog/', blog.urls, name="blog")
-#
-# Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
-
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("dashboard/", manoti.views.dahshboard, name="dashboard"),
-
+    url(r'^/', include('hello.urls')),
+    url(r'^crm/', include('manoti.urls')),
     path("admin/", admin.site.urls),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
