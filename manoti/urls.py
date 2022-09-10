5a29c01601113a6c7a70c5de7ca2890035ccf824
franck kavākuré <cynaptix@gmail.com>
2022-09-10 17:42:14 +0200

    Update urls.py
    adding contact related URLs

--- manoti/urls.py
+++ manoti/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^third-parties/$', views.third_party_homepage, name='third_party_homepage'),
     url(r'^third-party-list/$', views.list_third_parties, name='list_third_parties'),
     url(r'^third-party-view/(?P<thirdparty_id>[-\w]+)/$', views.third_party_view, name='third_party_view'),
+    url(r'^contact-list/$', views.list_contacts, name='list_contacts'),
+    url(r'^contact-view/(?P<contact_id>[-\w]+)/$', views.contact_view, name='contact_view'),
 ]
