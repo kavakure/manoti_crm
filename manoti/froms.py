8735e7571e8de16e30174d903b9864a0ff9890df
franck kavākuré <cynaptix@gmail.com>
2022-09-11 13:37:21 +0200

    Create froms.py

---
+++ manoti/froms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.utils.translation import ugettext, ugettext_lazy as _
+
+
+from django.forms.extras.widgets import SelectDateWidget
+from django.contrib.admin.widgets import AdminDateWidget
+
+from django.contrib.auth.models import User
+from .models import ThirdParty, Contact
+
+
+class ThirdPartyForm(forms.ModelForm):
+	class Meta:
+		model = ThirdParty
+		exclude = [
+			'vendor_code_number',
+			'customer_code_number',
+			'date_added'
+		]
