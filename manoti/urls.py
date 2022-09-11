464112f97cd5755f5469d6d145b1ae333d6d3dc0
franck kavākuré <cynaptix@gmail.com>
2022-09-11 13:38:03 +0200

    Update urls.py

--- manoti/urls.py
+++ manoti/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^third-party-list/$', views.list_third_parties, name='list_third_parties'),
     url(r'^third-party-list-filtered/(?P<thirdparty_type>[-\w]+)/$', views.filtered_list_third_parties, name='filtered_list_third_parties'),
     url(r'^third-party-view/(?P<thirdparty_id>[-\w]+)/$', views.third_party_view, name='third_party_view'),
+    url(r'^third-party-create/$', views.third_party_create, name='third_party_create'),
     url(r'^contact-list/$', views.list_contacts, name='list_contacts'),
     url(r'^contact_list-filtered/(?P<thirdparty_type>[-\w]+)/$', views.filtered_list_contact, name='filtered_list_contact'),
     url(r'^contact-view/(?P<contact_id>[-\w]+)/$', views.contact_view, name='contact_view'),
