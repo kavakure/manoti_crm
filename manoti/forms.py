34b4154a2e4ff2ec42d9107cf4519ed8491833d8
franck kavākuré <cynaptix@gmail.com>
2022-09-13 20:03:24 +0200

    Update forms.py
    hidding forms that will not be used further on

--- manoti/forms.py
+++ manoti/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import ugettext, ugettext_lazy as _
 
-from .utils import generate_customer_code, generate_vendor_code
+from .utils import generate_third_party_codes
 
 
 # from django.forms.extras.widgets import SelectDateWidget
@@ -13,12 +13,12 @@ from .models import ThirdParty, Contact
 
 class ThirdPartyForm(forms.ModelForm):
 
-	customer_code = forms.CharField(max_length=1000, label=_('Customer code'), initial=generate_customer_code(), widget=forms.TextInput(attrs={'class':'form-control'}))
-	vendor_code   = forms.CharField(max_length=1000, label=_('Vendor code'), initial=generate_vendor_code(), widget=forms.TextInput(attrs={'class':'form-control'}))
+	customer_code = forms.CharField(max_length=1000, label=_('Customer code'), initial=generate_third_party_codes()['customer_code'], widget=forms.TextInput(attrs={'class':'form-control'}))
+	vendor_code   = forms.CharField(max_length=1000, label=_('Vendor code'), initial=generate_third_party_codes()['vendor_code'], widget=forms.TextInput(attrs={'class':'form-control'}))
+	vendor_code_number  = forms.IntegerField(label=_('Vendor code number'), initial=generate_third_party_codes()['vendor_code_number'], widget=forms.TextInput(attrs={'class':'form-control', 'type':'hidden'}))
+	customer_code_number = forms.IntegerField(label=_('Customer code number'), initial=generate_third_party_codes()['customer_code_number'], widget=forms.TextInput(attrs={'class':'form-control', 'type':'hidden'}))
 	class Meta:
 		model = ThirdParty
 		exclude = [
-			'date_added',
-			'customer_code_number',
-			'vendor_code_number'
+			'date_added'
 		]
