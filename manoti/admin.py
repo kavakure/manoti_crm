df681239503db0aa6394cb3dc02788209d4a40d8
franck kavākuré <cynaptix@gmail.com>
2022-09-13 19:58:42 +0200

    Update admin.py
    jsut a simple listing tweak into the admin site

--- manoti/admin.py
+++ manoti/admin.py
@@ -2,13 +2,17 @@ from django.contrib import admin
 
 from manoti.models import *
 
+
+class ThirdPartyOptions(admin.ModelAdmin):
+	list_display = ('name', 'customer_code', 'customer_code_number', 'vendor_code', 'vendor_code_number', 'is_vendor')
+
 admin.site.register(Business)
 admin.site.register(BusinessAccountant)
 admin.site.register(SocialNetwork)
 admin.site.register(Employee)
 admin.site.register(BusinessEntityType)
 admin.site.register(ThirdPartyType)
-admin.site.register(ThirdParty)
+admin.site.register(ThirdParty, ThirdPartyOptions)
 admin.site.register(Title)
 admin.site.register(Contact)
 admin.site.register(PaymentType)
