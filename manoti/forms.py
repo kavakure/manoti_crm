01991553081d43d6008e1f9486b1481bc2b1f4cf
franck kavakure <cynaptix@gmail.com>
2022-09-13 15:39:16 +0200

    Update forms.py
    improving the required form

--- manoti/forms.py
+++ manoti/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.translation import ugettext, ugettext_lazy as _
 
+from .utils import generate_customer_code, generate_vendor_code
+
 
 # from django.forms.extras.widgets import SelectDateWidget
 from django.contrib.admin.widgets import AdminDateWidget
@@ -10,10 +12,13 @@ from .models import ThirdParty, Contact
 
 
 class ThirdPartyForm(forms.ModelForm):
+
+	customer_code = forms.CharField(max_length=1000, label=_('Customer code'), initial=generate_customer_code(), widget=forms.TextInput(attrs={'class':'form-control'}))
+	vendor_code   = forms.CharField(max_length=1000, label=_('Vendor code'), initial=generate_vendor_code(), widget=forms.TextInput(attrs={'class':'form-control'}))
 	class Meta:
 		model = ThirdParty
 		exclude = [
-			'vendor_code_number',
+			'date_added',
 			'customer_code_number',
-			'date_added'
+			'vendor_code_number'
 		]
