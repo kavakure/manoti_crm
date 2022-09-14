ff9f8f1958921f68d83d4b7bf8188e3232a7c95a
franck kavākuré <cynaptix@gmail.com>
2022-09-14 16:45:19 +0200

    Update forms.py
    documentating the form

--- manoti/forms.py
+++ manoti/forms.py
@@ -12,7 +12,7 @@ from .models import ThirdParty, Contact
 
 
 class ThirdPartyForm(forms.ModelForm):
-
+	"""Form used to create or edit a Third Party"""
 	customer_code = forms.CharField(max_length=1000, label=_('Customer code'), initial=generate_third_party_codes()['customer_code'], widget=forms.TextInput(attrs={'class':'form-control'}))
 	vendor_code   = forms.CharField(max_length=1000, label=_('Vendor code'), initial=generate_third_party_codes()['vendor_code'], widget=forms.TextInput(attrs={'class':'form-control'}))
 	vendor_code_number  = forms.IntegerField(label=_('Vendor code number'), initial=generate_third_party_codes()['vendor_code_number'], widget=forms.TextInput(attrs={'class':'form-control', 'type':'hidden'}))
