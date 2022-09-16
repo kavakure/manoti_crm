0a94fe8d3fb29500b00ccbf3a08c4236691a34b5
franck kavākuré <cynaptix@gmail.com>
2022-09-16 14:17:42 +0200

    Update admin.py
    registering new items in the admin site

--- manoti/admin.py
+++ manoti/admin.py
@@ -6,6 +6,14 @@ from manoti.models import *
 class ThirdPartyOptions(admin.ModelAdmin):
 	list_display = ('name', 'customer_code', 'customer_code_number', 'vendor_code', 'vendor_code_number', 'is_vendor')
 
+class  ProposalLine_Inline(admin.StackedInline):
+	model = ProposalLine
+	extra = 2
+
+class ProposalOptions(admin.ModelAdmin):
+	list_display = ('reference', 'customer_reference', 'third_party', 'timestamp', 'amount_incl_tax', 'status')
+	inlines = [ProposalLine_Inline]
+
 admin.site.register(Business)
 admin.site.register(BusinessAccountant)
 admin.site.register(SocialNetwork)
@@ -21,10 +29,11 @@ admin.site.register(Source)
 admin.site.register(AvailabilityDelay)
 admin.site.register(ShippingMetod)
 admin.site.register(ProposalDocumentTemplate)
-admin.site.register(Proposal)
+admin.site.register(Proposal, ProposalOptions)
 admin.site.register(PurchaseOrder)
 
 admin.site.register(BankAccount)
+admin.site.register(LineType)
 admin.site.register(VendorInvoice)
 admin.site.register(CustomerInvoice)
 admin.site.register(StatusChoices)
