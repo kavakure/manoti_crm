c0c4c22efb62505cbd6d80f7c06c0e30dd8a05a3
franck kavākuré <cynaptix@gmail.com>
2022-09-24 15:10:17 +0200

    Update urls.py
    adding banking area related URLS

--- manoti/urls.py
+++ manoti/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
 	url(r'^proposal-create/$', views.proposal_create, name='proposal_create'),
 	url(r'^proposal-edit/(?P<proposal_id>[-\w]+)/$', views.proposal_edit, name='proposal_edit'),
 	url(r'^proposal-delete/(?P<proposal_id>[-\w]+)/$', views.proposal_delete, name='proposal_delete'),
-    url(r'^proposal-clone/(?P<proposal_id>[-\w]+)/$', views.proposal_clone, name='proposal_clone'),
-    url(r'^proposal-set-status/(?P<proposal_id>[-\w]+)/$', views.proposal_set_status, name='proposal_set_status'),
+	url(r'^proposal-clone/(?P<proposal_id>[-\w]+)/$', views.proposal_clone, name='proposal_clone'),
+	url(r'^proposal-set-status/(?P<proposal_id>[-\w]+)/$', views.proposal_set_status, name='proposal_set_status'),
 	url(r'^proposal-toggle-validation/(?P<proposal_id>[-\w]+)/$', views.proposal_toggle_validation, name='proposal_toggle_validation'),
 	url(r'^proposal-toggle-billing/(?P<proposal_id>[-\w]+)/$', views.proposal_toggle_billing_status, name='proposal_toggle_billing_status'),
 	url(r'^proposal-line-add/(?P<proposal_id>[-\w]+)/$', views.proposal_line_add, name='proposal_line_add'),
@@ -36,10 +36,19 @@ urlpatterns = [
 	url(r'^proposal-linked-file-delete/(?P<proposal_id>[-\w]+)/(?P<linked_file_id>[-\w]+)/$', views.proposal_linked_file_delete, name='proposal_linked_file_delete'),
 	url(r'^proposal-attached-file-add/(?P<proposal_id>[-\w]+)/$', views.proposal_attached_file_add, name='proposal_attached_file_add'),
 	url(r'^proposal-attached-file-delete/(?P<proposal_id>[-\w]+)/(?P<attached_file_id>[-\w]+)/$', views.proposal_attached_file_delete, name='proposal_attached_file_delete'),
-    #Billing area ralated URLS
-    url(r'^billing/$', views.billing_homepage, name='billing_homepage'),
-    #Bank|cash area related URLS
-    url(r'^bank/$', views.bank_homepage, name='bank_homepage'),
-    url(r'^bank-view/(?P<bank_id>[-\w]+)/$', views.bank_view, name='bank_view'),
+	#Billing area ralated URLS
+	url(r'^billing/$', views.billing_homepage, name='billing_homepage'),
+	#Bank|cash area related URLS
+	url(r'^bank/$', views.bank_list, name='bank_list'),
+	url(r'^bank-view/(?P<bank_id>[-\w]+)/$', views.bank_view, name='bank_view'),
 	url(r'^bank-create/$', views.bank_create, name='bank_create'),
+	url(r'^bank-edit/(?P<bank_id>[-\w]+)/$', views.bank_edit, name='bank_edit'),
+	url(r'^bank-delete/(?P<bank_id>[-\w]+)/$', views.bank_delete, name='bank_delete'),
+	url(r'^bank-linked-file-add/(?P<bank_id>[-\w]+)/$', views.bank_linked_file_add, name='bank_linked_file_add'),
+	url(r'^bank-linked-file-delete/(?P<bank_id>[-\w]+)/(?P<linked_file_id>[-\w]+)/$', views.bank_linked_file_delete, name='bank_linked_file_delete'),
+	url(r'^bank-attached-file-add/(?P<bank_id>[-\w]+)/$', views.bank_attached_file_add, name='bank_attached_file_add'),
+	url(r'^bank-attached-file-delete/(?P<bank_id>[-\w]+)/(?P<attached_file_id>[-\w]+)/$', views.bank_attached_file_delete, name='bank_attached_file_delete'),
+	url(r'^bank-entries/$', views.bank_entry_list, name='bank_entry_list'),
+	url(r'^bank-entry-view/(?P<entry_id>[-\w]+)/$', views.bank_entry_view, name='bank_entry_view'),
+	url(r'^bank-entry-create/$', views.bank_entry_create, name='bank_entry_create'),
 ]
